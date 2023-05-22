11e503ba8cc34ded28339a6ce5e952944135ebb2
Lucid Kreaxions <chegekstephen@gmail.com>
2023-05-22 07:18:26 -0700

    hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 
 /**
- * Use the function puts
- * You are not allowwed to use printf
- * The program should end with the value 0
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
  */
 
 int main (void)
