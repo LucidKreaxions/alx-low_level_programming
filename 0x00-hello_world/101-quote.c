d7cbb74400836202fca9f16a3541bbf6b1eea8b9
Lucid Kreaxions <chegekstephen@gmail.com>
2023-05-25 16:09:35 -0700

    escaping special character

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
-#include <unistd.h>
 /*
- * Main - This program illustrates how to escape a special character.
+ * Main - Entry point
+ *
+ * This program illustrates how to escape a special character.
  *
  * Return: 1 Success
  */
