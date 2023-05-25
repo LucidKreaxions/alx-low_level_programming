b52a22ef407e98805a9106aaf185229dc3a5f1d8
Lucid Kreaxions <chegekstephen@gmail.com>
2023-05-25 16:01:52 -0700

    escaping special character

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,4 +1,10 @@
 #include <stdio.h>
+/*
+ * Main - This program illustrates how to escape a special character.
+ *
+ * Return: 1 Success
+ */
+
 int main(void)
 {
 	printf("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
