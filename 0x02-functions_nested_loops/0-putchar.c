89196de459b57962bcfce074a32c232388fe7796
Lucid Kreaxions <chegekstephen@gmail.com>
2023-06-01 09:43:12 -0700

    print _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * program that prints _putchar followed by \n
+ * Result: Always 0 (Success)
+ */
+int main()
+{
+	printf("_putchar\n");
+	return (0);
+}
